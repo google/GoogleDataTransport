008f50e39b3dd0d010b827c29a2b83303c5d0304
Maksym Malyhin <mmaksym@google.com>
2021-10-18 16:43:38 -0400

    GDTCORDroppedEventsCounter implementation and tests

--- GoogleDataTransport/GDTCORLibrary/ClientMetrics/GDTCORDroppedEventsCounter.m
+++ GoogleDataTransport/GDTCORLibrary/ClientMetrics/GDTCORDroppedEventsCounter.m
@@ -16,6 +16,60 @@
 
 #import "GoogleDataTransport/GDTCORLibrary/ClientMetrics/GDTCORDroppedEventsCounter.h"
 
+#import "GoogleDataTransport/GDTCORLibrary/Public/GoogleDataTransport/GDTCORConsoleLogger.h"
+
+static const NSInteger kCurrentStorageVersion = 1;
+
+static NSString *const kEventCountKey = @"eventCount";
+static NSString *const kDropReasonKey = @"dropReason";
+static NSString *const kMappingIDKey = @"mappingID";
+static NSString *const kStorageVersionKey = @"storageVersion";
+
 @implementation GDTCORDroppedEventsCounter
+
+- (instancetype)initWithEventCount:(NSInteger)eventCount
+                        dropReason:(GDTCOREventDropReason)dropReason
+                         mappingID:(NSString *)mappingID {
+  self = [super init];
+  if (self) {
+    _eventCount = eventCount;
+    _dropReason = dropReason;
+    _mappingID = mappingID;
+  }
+  return self;
+}
+
+- (NSUInteger)storageVersion {
+  return kCurrentStorageVersion;
+}
+
+#pragma mark - NSSecureCoding
+
++ (BOOL)supportsSecureCoding {
+  return YES;
+}
+
+- (void)encodeWithCoder:(nonnull NSCoder *)coder {
+  [coder encodeInteger:self.storageVersion forKey:kStorageVersionKey];
+  [coder encodeInteger:self.eventCount forKey:kEventCountKey];
+  [coder encodeInteger:self.dropReason forKey:kDropReasonKey];
+  [coder encodeObject:self.mappingID forKey:kMappingIDKey];
+}
+
+- (nullable instancetype)initWithCoder:(nonnull NSCoder *)coder {
+  NSInteger decodedStorageVersion = [coder decodeIntegerForKey:kStorageVersionKey];
+  if (decodedStorageVersion > self.storageVersion) {
+    GDTCORLogWarning(GDTCORMCDDebugLog,
+                     @"GDTCORDroppedEventsCounter was encoded by a newer coder version %ld. Current coder version is %ld. Some installation data may be lost.", decodedStorageVersion, kCurrentStorageVersion);
+    }
+
+  // Even if the mapping ID is corrupted, we still would like to capture the dropped event with an empty mapping ID.
+  NSString *mappingID = [coder decodeObjectOfClass:[NSString class] forKey:kMappingIDKey] ?: @"";
+  NSInteger evenCount = [coder decodeIntegerForKey:kEventCountKey];
+  GDTCOREventDropReason dropReason = [coder decodeIntegerForKey:kDropReasonKey];
+
+  return [self initWithEventCount:evenCount dropReason:dropReason mappingID:mappingID];
+
+}
 
 @end
---
+++ GoogleDataTransport/GDTCORTests/Unit/ClientMetrics/GDTCORDroppedEventsCounterTests.m
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <XCTest/XCTest.h>
+
+#import "GoogleDataTransport/GDTCORLibrary/ClientMetrics/GDTCORDroppedEventsCounter.h"
+
+#import "GoogleDataTransport/GDTCORLibrary/Internal/GDTCORPlatform.h"
+
+@interface GDTCORDroppedEventsCounterTests : XCTestCase
+
+@end
+
+@implementation GDTCORDroppedEventsCounterTests
+
+- (void)testCounterCanBeEncodedAndDecoded {
+  GDTCORDroppedEventsCounter *counter = [[GDTCORDroppedEventsCounter alloc] initWithEventCount:10 dropReason:GDTCOREventDropReasonStorageFull mappingID:@"some_id"];
+
+  NSData *encodedCounter = GDTCOREncodeArchive(counter, nil, nil);
+  XCTAssertNotNil(encodedCounter);
+
+  NSError *error;
+  GDTCORDroppedEventsCounter *decodedCounter = (GDTCORDroppedEventsCounter *)GDTCORDecodeArchive([GDTCORDroppedEventsCounter class], nil, encodedCounter, &error);
+
+  XCTAssertNotNil(decodedCounter);
+  XCTAssertTrue([decodedCounter isKindOfClass:[GDTCORDroppedEventsCounter class]]);
+  XCTAssertEqual(decodedCounter.eventCount, counter.eventCount);
+  XCTAssertEqual(decodedCounter.dropReason, counter.dropReason);
+  XCTAssertEqualObjects(decodedCounter.mappingID, counter.mappingID);
+}
+
+- (void)testCounterWithCorruptedMappingIDCanBeDecoded {
+  NSString *corruptedMappingID;
+  GDTCORDroppedEventsCounter *counter = [[GDTCORDroppedEventsCounter alloc] initWithEventCount:10 dropReason:GDTCOREventDropReasonStorageFull mappingID:corruptedMappingID];
+
+  NSData *encodedCounter = GDTCOREncodeArchive(counter, nil, nil);
+  XCTAssertNotNil(encodedCounter);
+
+  NSError *error;
+  GDTCORDroppedEventsCounter *decodedCounter = (GDTCORDroppedEventsCounter *)GDTCORDecodeArchive([GDTCORDroppedEventsCounter class], nil, encodedCounter, &error);
+
+  XCTAssertNotNil(decodedCounter);
+  XCTAssertTrue([decodedCounter isKindOfClass:[GDTCORDroppedEventsCounter class]]);
+  XCTAssertEqual(decodedCounter.eventCount, counter.eventCount);
+  XCTAssertEqual(decodedCounter.dropReason, counter.dropReason);
+  XCTAssertEqualObjects(decodedCounter.mappingID, @"");
+}
+
+@end
