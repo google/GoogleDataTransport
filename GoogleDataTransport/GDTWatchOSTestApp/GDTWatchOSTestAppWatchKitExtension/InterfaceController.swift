82ad8c0804e532836522bef07c1adcd34f4f1e4b
Nick Cooke <nickcooke@google.com>
2022-01-26 17:19:49 -0500

    Fix watchOS failure

--- GoogleDataTransport/GDTWatchOSTestApp/GDTWatchOSTestAppWatchKitExtension/InterfaceController.swift
+++ GoogleDataTransport/GDTWatchOSTestApp/GDTWatchOSTestAppWatchKitExtension/InterfaceController.swift
@@ -21,8 +21,7 @@ import Dispatch
 import GoogleDataTransport
 
 class InterfaceController: WKInterfaceController {
-  let transport: GDTCORTransport = .init(mappingID: "1234", transformers: nil,
-                                         target: GDTCORTarget.test)!
+  let transport = GDTCORTransport(mappingID: "1234", transformers: nil, target: GDTCORTarget.test)!
 
   override func awake(withContext context: Any?) {
     super.awake(withContext: context)
