3e5156ae7da2214f1ad6eaa4dd2fc06e4a89479a
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2023-05-04 15:12:20 -0400

    [Fix] Address Xcode 14.3 Analyzer issue (#95)
    * [Fix] Address Xcode 14.3 Analyzer issue
    
    * Add changelog entry
    
    * Fix *all* warnings
    
    * check in Gemfile.lock
    
    * Style

--- GoogleDataTransport/GDTCORLibrary/GDTCORPlatform.m
+++ GoogleDataTransport/GDTCORLibrary/GDTCORPlatform.m
@@ -205,8 +205,12 @@ NSData *_Nullable GDTCOREncodeArchive(id<NSSecureCoding> obj,
     }
     if (filePath.length > 0) {
       result = [resultData writeToFile:filePath options:NSDataWritingAtomic error:error];
-      if (result == NO || *error) {
-        GDTCORLogDebug(@"Attempt to write archive failed: path:%@ error:%@", filePath, *error);
+      if (result == NO || (error != NULL && *error != nil)) {
+        if (error != NULL && *error != nil) {
+          GDTCORLogDebug(@"Attempt to write archive failed: path:%@ error:%@", filePath, *error);
+        } else {
+          GDTCORLogDebug(@"Attempt to write archive failed: path:%@", filePath);
+        }
       } else {
         GDTCORLogDebug(@"Writing archive succeeded: %@", filePath);
       }
