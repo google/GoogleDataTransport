d4932cd78106741ed38ddcd83f1207fc9ff79a3a
Nick Cooke <36927374+ncooke3@users.noreply.github.com>
2024-05-27 11:37:22 -0400

    [NFC] Prefer <dispatch/dispatch.h> over <dispatch/time.h> (#131)

--- third_party/GDTCORClockUtils/GDTCORClockUtils.m
+++ third_party/GDTCORClockUtils/GDTCORClockUtils.m
@@ -1,6 +1,6 @@
 #import <sys/sysctl.h>
 
-#include <dispatch/time.h>
+#include <dispatch/dispatch.h>
 
 // Using a monotonic clock is necessary because CFAbsoluteTimeGetCurrent(), NSDate, and related all
 // are subject to drift. That it to say, multiple consecutive calls do not always result in a
