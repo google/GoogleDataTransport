458340a5fbdec8da7d17b5e45f6b2d9261306093
Nick Cooke <nickcooke@google.com>
2024-06-12 10:37:57 -0400

    Add renames

--- GoogleDataTransport/GDTCCTLibrary/Private/NSURLSession+GDTCCTPromises.h
+++ GoogleDataTransport/GDTCCTLibrary/Private/NSURLSession+GDTCCTPromises.h
@@ -1,12 +1,35 @@
+// Copyright 2024 Google LLC
 //
-//  NSURLSession+GDTCCTPromises.h
-//  GCDWebServer
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Nick Cooke on 6/12/24.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef NSURLSession_GDTCCTPromises_h
-#define NSURLSession_GDTCCTPromises_h
+#import <Foundation/Foundation.h>
 
+@class FBLPromise<Value>;
+@class GDTCCTURLSessionDataResponse;
 
-#endif /* NSURLSession_GDTCCTPromises_h */
+NS_ASSUME_NONNULL_BEGIN
+
+/** Promise based API for `NSURLSession`. */
+@interface NSURLSession (GDTCCTPromises)
+
+/** Creates a promise wrapping `-[NSURLSession dataTaskWithRequest:completionHandler:]` method.
+ * @param URLRequest The request to create a data task with.
+ * @return A promise that is fulfilled when an HTTP response is received (with any response code),
+ * or is rejected with the error passed to the task completion.
+ */
+- (FBLPromise<GDTCCTURLSessionDataResponse *> *)gdtcct_dataTaskPromiseWithRequest:
+    (NSURLRequest *)URLRequest;
+
+@end
+
+NS_ASSUME_NONNULL_END
